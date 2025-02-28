c6086245bfb77fad95ced8b5ca0eadcd9f2ae3f1
Elda Crespo <crespoelda@gmail.com>
2025-02-28 13:21:03 +0100

    Ejercicios 5 y 6

--- Elda/ejerciciosPrueba/BDAviones.sql
+++ Elda/ejerciciosPrueba/BDAviones.sql
@@ -60,6 +60,79 @@ CREATE TABLE IF NOT EXISTS `BDAviones`.`Vuelo` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `BDAviones`.`Pasajeros`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BDAviones`.`Pasajeros` (
+  `Dni` INT NOT NULL,
+  `nombrePasajero` VARCHAR(45) NOT NULL,
+  `ApellidosPasajero` VARCHAR(45) NOT NULL,
+  `direcciónPasajero` VARCHAR(45) NULL,
+  `teléfonoPasajero` VARCHAR(45) NULL,
+  PRIMARY KEY (`Dni`),
+  UNIQUE INDEX `Dni_UNIQUE` (`Dni` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BDAviones`.`Vuelo_has_Pasajeros`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BDAviones`.`Vuelo_has_Pasajeros` (
+  `Vuelo_idVuelo` INT NOT NULL,
+  `Vuelo_Avion_matricula` INT NOT NULL,
+  `Pasajeros_Dni` INT NOT NULL,
+  PRIMARY KEY (`Vuelo_idVuelo`, `Vuelo_Avion_matricula`, `Pasajeros_Dni`),
+  INDEX `fk_Vuelo_has_Pasajeros_Pasajeros1_idx` (`Pasajeros_Dni` ASC) VISIBLE,
+  INDEX `fk_Vuelo_has_Pasajeros_Vuelo1_idx` (`Vuelo_idVuelo` ASC, `Vuelo_Avion_matricula` ASC) VISIBLE,
+  CONSTRAINT `fk_Vuelo_has_Pasajeros_Vuelo1`
+    FOREIGN KEY (`Vuelo_idVuelo` , `Vuelo_Avion_matricula`)
+    REFERENCES `BDAviones`.`Vuelo` (`idVuelo` , `Avion_matricula`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Vuelo_has_Pasajeros_Pasajeros1`
+    FOREIGN KEY (`Pasajeros_Dni`)
+    REFERENCES `BDAviones`.`Pasajeros` (`Dni`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BDAviones`.`Pilotos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BDAviones`.`Pilotos` (
+  `númeroPiloto` INT NOT NULL,
+  `nombrePiloto` VARCHAR(45) NULL,
+  `apellidosPiloto` VARCHAR(45) NULL,
+  `direcciónPiloto` VARCHAR(45) NULL,
+  `teléfonoPiloto` VARCHAR(45) NULL,
+  PRIMARY KEY (`númeroPiloto`),
+  UNIQUE INDEX `númeroPiloto_UNIQUE` (`númeroPiloto` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `BDAviones`.`Pilotos_has_Avion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BDAviones`.`Pilotos_has_Avion` (
+  `Pilotos_númeroPiloto` INT NOT NULL,
+  `Avion_matricula` INT NOT NULL,
+  PRIMARY KEY (`Pilotos_númeroPiloto`, `Avion_matricula`),
+  INDEX `fk_Pilotos_has_Avion_Avion1_idx` (`Avion_matricula` ASC) VISIBLE,
+  INDEX `fk_Pilotos_has_Avion_Pilotos1_idx` (`Pilotos_númeroPiloto` ASC) VISIBLE,
+  CONSTRAINT `fk_Pilotos_has_Avion_Pilotos1`
+    FOREIGN KEY (`Pilotos_númeroPiloto`)
+    REFERENCES `BDAviones`.`Pilotos` (`númeroPiloto`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Pilotos_has_Avion_Avion1`
+    FOREIGN KEY (`Avion_matricula`)
+    REFERENCES `BDAviones`.`Avion` (`matricula`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
