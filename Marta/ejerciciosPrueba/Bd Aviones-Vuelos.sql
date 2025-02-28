59eaddf084d300cc27335631db0e2aa2dc3a8f8d
MartaLamas. <gonzalezlamasmarta@gmail.com>
2025-02-28 13:21:03 +0100

    Ejercicio 5 y 6.

--- Marta/ejerciciosPrueba/Bd Aviones-Vuelos.sql
+++ Marta/ejerciciosPrueba/Bd Aviones-Vuelos.sql
@@ -60,6 +60,78 @@ CREATE TABLE IF NOT EXISTS `bdAVIONES-VUELOS`.`Avión` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `bdAVIONES-VUELOS`.`PASAJEROS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bdAVIONES-VUELOS`.`PASAJEROS` (
+  `dniPasajeros` INT NOT NULL,
+  `nombrePasajeros` VARCHAR(45) NULL,
+  `apellidosPasajeros` VARCHAR(45) NULL,
+  `dirección` VARCHAR(45) NULL,
+  `tfnoPasajeros` VARCHAR(45) NULL,
+  PRIMARY KEY (`dniPasajeros`),
+  UNIQUE INDEX `dniPasajeros_UNIQUE` (`dniPasajeros` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `bdAVIONES-VUELOS`.`PASAJEROS_has_VUELOS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bdAVIONES-VUELOS`.`PASAJEROS_has_VUELOS` (
+  `PASAJEROS_dniPasajeros` INT NOT NULL,
+  `VUELOS_idVuelo` INT NOT NULL,
+  PRIMARY KEY (`PASAJEROS_dniPasajeros`, `VUELOS_idVuelo`),
+  INDEX `fk_PASAJEROS_has_VUELOS_VUELOS1_idx` (`VUELOS_idVuelo` ASC) VISIBLE,
+  INDEX `fk_PASAJEROS_has_VUELOS_PASAJEROS1_idx` (`PASAJEROS_dniPasajeros` ASC) VISIBLE,
+  CONSTRAINT `fk_PASAJEROS_has_VUELOS_PASAJEROS1`
+    FOREIGN KEY (`PASAJEROS_dniPasajeros`)
+    REFERENCES `bdAVIONES-VUELOS`.`PASAJEROS` (`dniPasajeros`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PASAJEROS_has_VUELOS_VUELOS1`
+    FOREIGN KEY (`VUELOS_idVuelo`)
+    REFERENCES `bdAVIONES-VUELOS`.`VUELOS` (`idVuelo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `bdAVIONES-VUELOS`.`PILOTOS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bdAVIONES-VUELOS`.`PILOTOS` (
+  `idPILOTOS` INT NOT NULL,
+  `nombrePiloto` VARCHAR(45) NULL,
+  `apellidosPiloto` VARCHAR(45) NULL,
+  `direccionPilotos` VARCHAR(45) NULL,
+  PRIMARY KEY (`idPILOTOS`),
+  UNIQUE INDEX `idPILOTOS_UNIQUE` (`idPILOTOS` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `bdAVIONES-VUELOS`.`Avión_has_PILOTOS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bdAVIONES-VUELOS`.`Avión_has_PILOTOS` (
+  `Avión_idAvión` INT NOT NULL,
+  `Avión_VUELOS_idVuelo` INT NOT NULL,
+  `PILOTOS_idPILOTOS` INT NOT NULL,
+  PRIMARY KEY (`Avión_idAvión`, `Avión_VUELOS_idVuelo`, `PILOTOS_idPILOTOS`),
+  INDEX `fk_Avión_has_PILOTOS_PILOTOS1_idx` (`PILOTOS_idPILOTOS` ASC) VISIBLE,
+  INDEX `fk_Avión_has_PILOTOS_Avión1_idx` (`Avión_idAvión` ASC, `Avión_VUELOS_idVuelo` ASC) VISIBLE,
+  CONSTRAINT `fk_Avión_has_PILOTOS_Avión1`
+    FOREIGN KEY (`Avión_idAvión` , `Avión_VUELOS_idVuelo`)
+    REFERENCES `bdAVIONES-VUELOS`.`Avión` (`idAvión` , `VUELOS_idVuelo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Avión_has_PILOTOS_PILOTOS1`
+    FOREIGN KEY (`PILOTOS_idPILOTOS`)
+    REFERENCES `bdAVIONES-VUELOS`.`PILOTOS` (`idPILOTOS`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
