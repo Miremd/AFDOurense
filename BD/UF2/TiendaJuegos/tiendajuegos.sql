53670a1e95a0b12aaa2ad5140b62de1464bec911
Daniel Álvarez Domínguez <noveascomomedescojono@gmail.com>
2025-03-17 13:06:45 +0100

    Update tiendajuegos.sql

--- BD/UF2/TiendaJuegos/tiendajuegos.sql
+++ BD/UF2/TiendaJuegos/tiendajuegos.sql
@@ -1,12 +1,31 @@
 create database if not exists tiendajuegos;
 use tiendajuegos;
 
-create table juegos (
+-- datos de tabla juegos
+create table if not exists juegos (
 	idJuego int,
 	nombreJuego varchar(50)
 );
+
+-- Modificamos Nombres de campos
+alter table juegos
+change column idJuego ID int;
+alter table juegos
+change column nombreJuego nombre varchar(50);
+
+-- Añadimos 3 columnas nuevas
+alter table juegos
+add column categoria enum('puzzle', 'lucha','estrategia','roguelike');
+alter table juegos
+add column precio double(5,2);
+alter table juegos
+add column stock int;
+
+-- Ponemos Clave primaria
+Alter table juegos
+add constraint auto_increment primary key (ID);
                     
-CREATE TABLE Pedidos (
+CREATE TABLE if not exists Pedidos (
 	idPedido INT
 
 );
@@ -18,9 +37,7 @@ create table if not exists Clientes (
 );
 
 
-CREATE TABLE detalle_pedidos (
+CREATE TABLE if not exists detalle_pedidos (
     id_detalle int primary key auto_increment
     
 );
-
-
