bdb166991f290b8c050843ad027e9ee11d871301
MartaLamas. <gonzalezlamasmarta@gmail.com>
2025-03-17 12:52:17 +0100

    Añadir y modificar columnas tabla Pedidos

--- BD/UF2/TiendaJuegos/tiendajuegos.sql
+++ BD/UF2/TiendaJuegos/tiendajuegos.sql
@@ -5,11 +5,23 @@ create table juegos (
 	idJuego int,
 	nombreJuego varchar(50)
 );
-                    
-CREATE TABLE Pedidos (
-	idPedido INT
+      
+      -- Creamos tabla Pedidos
+CREATE TABLE IF NOT EXISTS Pedidos (
+	idPedido INT 
+    
 
 );
+-- Usamos el ALTER TABLE, a ver que pasa...
+
+ALTER TABLE Pedidos 
+CHANGE COLUMN IdPedido ID INT;
+ALTER TABLE Pedidos
+MODIFY COLUMN ID INT AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE Pedidos 
+ADD COLUMN ID_cliente INT;
+ALTER TABLE Pedidos
+ADD COLUMN fecha_pedido DATE;
 
 -- Crear tabla Clientes
 create table if not exists Clientes (
