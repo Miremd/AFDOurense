a2afbd39862f3ced7ee4919ebca1690e945ae114
Elda Crespo <crespoelda@gmail.com>
2025-03-17 12:45:53 +0100

    Añadir y modificar columnas en la tabla Clientes

--- BD/UF2/TiendaJuegos/tiendajuegos.sql
+++ BD/UF2/TiendaJuegos/tiendajuegos.sql
@@ -16,7 +16,16 @@ create table if not exists Clientes (
 	idCliente int, 
     nombre varchar(50)
 );
+--  Modificaciones de la tabla Clientes
 
+alter table Clientes 
+change column idCliente ID int;
+alter table Clientes
+modify column ID int primary key auto_increment;
+alter table Clientes
+add column email varchar(50);
+alter table Clientes
+add column fechaRegistro date;
 
 CREATE TABLE detalle_pedidos (
     id_detalle int primary key auto_increment
